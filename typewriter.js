14adc73c47a878adf0df43bf4ea9a518b5a4f55a
Farito <182032609+22Farito@users.noreply.github.com>
2025-11-09 07:20:56 +0000

    Update typewriter.js

--- typewriter.js
+++ typewriter.js
@@ -1,12 +1,25 @@
 const text = "Developer";
 const typewriter = document.getElementById("typewriter");
 let index = 0;
+let forward = true;
 
 function type() {
-  typewriter.textContent = text.slice(0, index);
-  index++;
-  if (index > text.length) index = 0; // loop continuously
-  setTimeout(type, 200); // speed of typing
+  if (forward) {
+    typewriter.textContent = text.slice(0, index + 1);
+    index++;
+    if (index === text.length) {
+      forward = false;
+      setTimeout(type, 500); // pause 0.5s at the end
+      return;
+    }
+  } else {
+    typewriter.textContent = text.slice(0, index);
+    index--;
+    if (index < 0) {
+      forward = true;
+    }
+  }
+  setTimeout(type, 200);
 }
 
 type();
